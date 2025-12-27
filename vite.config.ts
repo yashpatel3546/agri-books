b36bfc39dbcf8da735aebfa97aae13a041d10c2a
yashpatel3546 <patelyash2262@gmail.com>
2025-12-27 19:06:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ----------------------------------------------------
+      // THIS IS THE MISSING PART THAT FIXES THE WHITE SCREEN
+      base: "/agri-books/",
+      // ----------------------------------------------------
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
