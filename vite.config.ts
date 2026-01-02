2a7e5ab25f9cb204181ea79a42b4dc019fff9f15
yashpatel3546 <patelyash2262@gmail.com>
2026-01-02 17:47:18 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -6,12 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // START OF FIX --------------------------------------
-      // This line prevents the White Screen. 
-      // It MUST match your GitHub repository name exactly.
-      base: "/agri-books/", 
-      // END OF FIX ----------------------------------------
-
       server: {
         port: 3000,
         host: '0.0.0.0',
